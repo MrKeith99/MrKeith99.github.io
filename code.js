f0c2960183ac2b984a8357abc53d67bec5834f45
MrKeith99 <68671699+MrKeith99@users.noreply.github.com>
2020-07-28 19:14:32 +0900

    Add files via upload

---
+++ code.js
@@ -0,0 +1,157 @@
+$(".pers11").hide();
+$(".pers12").hide();
+$(".pers13").hide();
+$(".pers21").hide();
+$(".pers22").hide();
+$(".pers23").hide();
+$(".pers31").hide();
+$(".pers32").hide();
+$(".pers33").hide();
+$(".pers41").hide();
+$(".pers42").hide();
+$(".pers43").hide();
+
+$("#m11").click(function(){
+    var x = document.getElementById("m11");
+    
+    if(x.innerHTML === "＋MORE"){
+        x.innerHTML = "ーLESS";
+        $(".pers11").fadeToggle(1000);
+    }
+    else if(x.innerHTML === "ーLESS"){
+        x.innerHTML = "＋MORE";
+        $(".pers11").hide();
+    }
+});
+$("#m12").click(function(){
+    var x = document.getElementById("m12");
+    
+    if(x.innerHTML === "＋MORE"){
+        x.innerHTML = "ーLESS";
+        $(".pers12").fadeToggle(1000);
+    }
+    else if(x.innerHTML === "ーLESS"){
+        x.innerHTML = "＋MORE";
+        $(".pers12").hide();
+    }
+});
+$("#m13").click(function(){
+    var x = document.getElementById("m13");
+    
+    if(x.innerHTML === "＋MORE"){
+        x.innerHTML = "ーLESS";
+        $(".pers13").fadeToggle(1000);
+    }
+    else if(x.innerHTML === "ーLESS"){
+        x.innerHTML = "＋MORE";
+        $(".pers13").hide();
+    }
+});
+$("#m21").click(function(){
+    var x = document.getElementById("m21");
+    
+    if(x.innerHTML === "＋MORE"){
+        x.innerHTML = "ーLESS";
+        $(".pers21").fadeToggle(1000);
+    }
+    else if(x.innerHTML === "ーLESS"){
+        x.innerHTML = "＋MORE";
+        $(".pers21").hide();
+    }
+});
+$("#m22").click(function(){
+    var x = document.getElementById("m22");
+    
+    if(x.innerHTML === "＋MORE"){
+        x.innerHTML = "ーLESS";
+        $(".pers22").fadeToggle(1000);
+    }
+    else if(x.innerHTML === "ーLESS"){
+        x.innerHTML = "＋MORE";
+        $(".pers22").hide();
+    }
+});
+$("#m23").click(function(){
+    var x = document.getElementById("m23");
+    
+    if(x.innerHTML === "＋MORE"){
+        x.innerHTML = "ーLESS";
+        $(".pers23").fadeToggle(1000);
+    }
+    else if(x.innerHTML === "ーLESS"){
+        x.innerHTML = "＋MORE";
+        $(".pers23").hide();
+    }
+});
+$("#m31").click(function(){
+    var x = document.getElementById("m31");
+    
+    if(x.innerHTML === "＋MORE"){
+        x.innerHTML = "ーLESS";
+        $(".pers31").fadeToggle(1000);
+    }
+    else if(x.innerHTML === "ーLESS"){
+        x.innerHTML = "＋MORE";
+        $(".pers31").hide();
+    }
+});
+$("#m32").click(function(){
+    var x = document.getElementById("m32");
+    
+    if(x.innerHTML === "＋MORE"){
+        x.innerHTML = "ーLESS";
+        $(".pers32").fadeToggle(1000);
+    }
+    else if(x.innerHTML === "ーLESS"){
+        x.innerHTML = "＋MORE";
+        $(".pers32").hide();
+    }
+});
+$("#m33").click(function(){
+    var x = document.getElementById("m33");
+    
+    if(x.innerHTML === "＋MORE"){
+        x.innerHTML = "ーLESS";
+        $(".pers33").fadeToggle(1000);
+    }
+    else if(x.innerHTML === "ーLESS"){
+        x.innerHTML = "＋MORE";
+        $(".pers33").hide();
+    }
+});
+$("#m41").click(function(){
+    var x = document.getElementById("m41");
+    
+    if(x.innerHTML === "＋MORE"){
+        x.innerHTML = "ーLESS";
+        $(".pers41").fadeToggle(1000);
+    }
+    else if(x.innerHTML === "ーLESS"){
+        x.innerHTML = "＋MORE";
+        $(".pers41").hide();
+    }
+});
+$("#m42").click(function(){
+    var x = document.getElementById("m42");
+    
+    if(x.innerHTML === "＋MORE"){
+        x.innerHTML = "ーLESS";
+        $(".pers42").fadeToggle(1000);
+    }
+    else if(x.innerHTML === "ーLESS"){
+        x.innerHTML = "＋MORE";
+        $(".pers42").hide();
+    }
+});
+$("#m43").click(function(){
+    var x = document.getElementById("m43");
+    
+    if(x.innerHTML === "＋MORE"){
+        x.innerHTML = "ーLESS";
+        $(".pers43").fadeToggle(1000);
+    }
+    else if(x.innerHTML === "ーLESS"){
+        x.innerHTML = "＋MORE";
+        $(".pers43").hide();
+    }
+});
